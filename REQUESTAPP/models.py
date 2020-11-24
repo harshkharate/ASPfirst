49796451af474d0cd3147ff601a56534dbc9cac3
harshkharate <harshkharate@mail.com>
2020-11-24 16:49:23 -0600

    added users and model

--- REQUESTAPP/models.py
+++ REQUESTAPP/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Request(models.Model):
+    Name              =models.CharField(max_length=80)
+    Username          =models.CharField(max_length=20)
+    Email             =models.EmailField()
+    Department        =models.CharField(max_length=20)
+    Project           =models.CharField(max_length=40)
+    Manager           =models.CharField(max_length=80)
+    Justification     =models.TextField()
+    StartDate         =models.DateField()
+    AccessDuration    =models.AutoField()
+    S3Bucket       =models.CharField(max_length=80)
+    Permission   =models.CharField(max_length=20, default='Read/Write')
